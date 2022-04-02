fb4b73cf7337f6559301ed3500be357097d14fc0
MrDomino <dominicjaymia@gmail.com>
2022-04-02 12:43:13 +0100

    renamed "colours" to "attributes"

--- attributes.py
+++ attributes.py
@@ -1,6 +1,6 @@
 import _curses as curses
 
-class Colours:
+class Attributes:
 	def __init__(self):
 		curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
 		curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
